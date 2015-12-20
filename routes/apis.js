2727412ca004fc13ec231ba9b84705ce958b6b3e
garyleetw <gary.lee.tw@gmail.com>
2015-12-20 11:25:41 +0800

    API implementation step 1

--- routes/apis.js
+++ routes/apis.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+var db =[];
+
 /* RESP APIs */
-router.post( '/2/post', function(req, res, next) {
-	var person;
-	
-	person = {
-		'name': 'Gary',
-		'e-mail': 'gary.lee.tw@gmail.com'
+router.post( '/1/post', function(req, res, next) {
+	var title = req.query.title;
+	var message = req.query.message;
+
+    //Use object
+	var article = {
+		title:title,
+		message:message
 	};
 
-	res.json(person);
+	db.push(article);
+	
+	res.json({
+		status: "OK"
+		});
 });
 
-router.get( '/2/post/:id', function(req, res, next) {
-	id = req.params.id
-	
-	res.end();
+router.get( '/1/post/', function(req, res, next) {
+	res.json(db);
 });
 
 module.exports = router;
